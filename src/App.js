9ef90f0871cb41b08cd8319319f6786a53fa887b
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-05-27 01:15:09 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    // const fetchProd = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products/")
+    //   const data = await res.json();
+    //   console.log(data);
+    //   setProduct(data);
+    // };
+    // fetchProd();
+
+      fetch("https://restcountries.com/v3.1/region/europe")
+        .then((res) => res.json())
+        .then((data) => {
+          setProduct(data);
+          console.log(data);
+        })
+        .catch((err) => alert(err.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grid grid-cols-4">
+
+        {product.map((allItems) => {
+          return (
+            <div className=" h-96 border-4 overflow-scroll" key={allItems.id}>
+              <img src={allItems.flags.png} alt="COUNTRY-FLAG" className=" h-56" />
+              <div>
+                <h2 className="text-xl">
+                  <span className="font-bold text-red-600">Name:</span>{" "}
+                  {allItems.name.common}
+                </h2>
+                <p>
+                  <span className="font-bold text-red-600">capital:</span>
+                  {allItems.capital}
+                </p>
+                <p>
+                  <span className="font-bold text-red-600">Population:</span>
+                  {allItems.population}
+                </p>
+                <p>
+                  <span className="font-bold text-red-600">Subregion :</span>
+                  {allItems.subregion}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
